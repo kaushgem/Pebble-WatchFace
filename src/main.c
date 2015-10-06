f62a413aafe31d5f3ded15b878c2ecab8efeee24
Kaushik <kaushgem@users.noreply.github.com>
2015-10-05 23:38:50 -0400

    Custom font

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@ Simple WatchFace
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+//Custom font
+static GFont s_time_font;
+
 //Method to update time
 static void update_time() {
   time_t temp = time(NULL);
@@ -31,20 +34,24 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 //Window load
 static void main_window_load(Window *window) {
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  //Time layer
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+  //s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  text_layer_set_font(s_time_layer, s_time_font);
+  //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+
   layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_time_layer));
 }
 
 //Window Unload
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
 }
 
 //Program load
